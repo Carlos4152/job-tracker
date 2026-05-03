a87684b22c1931b71c52e677dd5dca7d2e606b9f
Carlos4152 <lopez.carlos.ct@gmail.com>
2026-05-03 08:36:14 -0400

    fixed: remove unuse file along with test content on app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import { ResetPasswordEmail } from '@/lib/email/templates/reset-password'
+
 
 export default function Home() {
-  return (
-    <ResetPasswordEmail
-      clientName="Jordan Davis"
-      resetUrl="https://jobtrack.com/reset-password?token=xxx"
-      ipAddress="192.168.1.1"
-      expiresIn="24 hours"
-    />
-  )
+  return <div></div>;
 }
