1d20cb224eabbe04cfdf71cf95e3eb1f903afdc8
Carlos4152 <lopez.carlos.ct@gmail.com>
2026-05-06 11:21:02 -0400

    feat: update the cover image for authlayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,25 @@
 import { Box, Grid, GridItem } from '@chakra-ui/react';
+import Image from 'next/image';
 
 function Authlayout({ children }: { children: React.ReactNode }) {
   return (
     <Grid
-      templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(2, 1fr)' }}
+      templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(12, 1fr)' }}
       h="100vh"
     >
-      <GridItem py={10}>{children}</GridItem>
-      <GridItem display={{ base: 'none', lg: 'block' }}>
-        <Box bgColor="fg.subtle/10" h="full" />
+      <GridItem py={10} colSpan={{ lg: 5}}>{children}</GridItem>
+      <GridItem
+        display={{ base: 'none', lg: 'block' }}
+        position="relative" 
+        colSpan={{ lg: 7}}
+      >
+        <Image
+          src="/auth-img.png"
+          alt="Developer Searching for jobs"
+          fill
+          style={{ objectFit: 'cover' }}
+          priority
+        />
       </GridItem>
     </Grid>
   );
